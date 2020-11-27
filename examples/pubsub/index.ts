3ec6d4cdd0286c427d88fcdf4a34cead0fdc4a39
Richard Hofmänner <hello@terillos.dev>
2020-11-27 22:46:53 +0100

    Added Support for the PubSub chat_moderator_actions topic

--- examples/pubsub/index.ts
+++ examples/pubsub/index.ts
@@ -1,3 +1,21 @@
+import { topic } from "../../definitions/pubsub/topics.ts"
 import {PubSub} from "../../mod.ts"
+import {dotEnvLoad} from "../../deps.ts"
+import { PubSubMessage } from "../../definitions/pubsub/PubSubMessage.ts"
 
-const ws = new PubSub()
+dotEnvLoad({
+    dotEnvPath: '/home/richard/Desktop/workspace/twitchd/.env'
+})
+
+const ws = new PubSub({
+    oAuthToken: Deno.env.get("O_AUTH_TOKEN") || "",
+    topics: [topic.chat_moderator_actions],
+    channel_id: parseInt(Deno.env.get("CHANNEL_ID") || ""),
+    user_id: parseInt(Deno.env.get("USER_ID") || "")
+})
+
+ws.on("connected", () => console.log("connected"))
+ws.on("error", (error: string) => console.log(error))
+ws.on("message", (message: PubSubMessage) => {
+    console.log('There is a new Message', message.data?.message)
+})
--- pubsub/PubSub.ts
+++ pubsub/PubSub.ts
@@ -1,15 +1,71 @@
 import {WebSocketClient} from "../deps.ts"
+import {PubSubConstructor, topic} from "../definitions/pubsub/topics.ts"
+import {PubSubMessage, PubSubMessageType} from "../definitions/pubsub/PubSubMessage.ts"
+import EventEmitter from "https://deno.land/std@0.65.0/node/events.ts"
 
-export class PubSub {
+export class PubSub extends EventEmitter {
     readonly wwsEndpoint: string = "wss://pubsub-edge.twitch.tv"
-    socket: WebSocketClient
+    private socket: WebSocketClient
+    private oAuthToken: string
+    private topics: topic[]
+    private user_id?: number
+    private channel_id?: number
 
-    constructor() {
+    /**
+     * Create a connection to the PubSub Endpoint and listen to events
+     * 
+     * @param {PubSubConstructor} PubSubConstructor 
+     */
+    constructor(params: PubSubConstructor) {
+        super()
+        this.oAuthToken = params.oAuthToken
+        this.topics = params.topics
+        this.user_id = params.user_id
+        this.channel_id = params.channel_id
         this.socket = new WebSocketClient(this.wwsEndpoint)
-        this.socket.on("message", this.wsOnMessageHandler)
+
+        this.socket.on("open", () => this.wsOnOpenHandler())
+        this.socket.on("message",(message: string) => this.wsOnMessageHandler(message))
+        this.socket.on("error", (error: string) => this.emit("error", error))
+    }
+
+    private wsOnOpenHandler() {
+        let topicsArray: Array<string> = new Array()
+        this.topics.forEach((element) => {
+            switch (element) {
+                case topic.chat_moderator_actions:
+                    topicsArray.push(`chat_moderator_actions.${this.user_id}.${this.channel_id}`)
+                    break;
+                default:
+                    break;
+            }
+
+            this.socket.send(`{
+                "type": "LISTEN",
+                "data": {
+                    "topics": ["chat_moderator_actions.${this.user_id}.${this.channel_id}"],
+                    "auth_token": "${this.oAuthToken}"
+                }
+            }`)
+
+            this.emit("connected")
+        })
     }
 
     private wsOnMessageHandler(message: string){
-        console.log(message)
+        const msg: PubSubMessage = JSON.parse(message)
+        
+        if (msg.type === PubSubMessageType.PING) {
+            this.socket.send('{"type": "PONG"}')
+        } else if (msg.type === PubSubMessageType.RECONNECT) {
+            this.socket.close()
+            this.socket = new WebSocketClient(this.wwsEndpoint)
+        } else if (msg.type === PubSubMessageType.RESPONSE) {
+            if (msg.data !== undefined && msg.data.error !== '') {
+                this.emit("error", "There was some error.")
+            }
+        } else {
+            this.emit("message", msg)
+        }
     }
 }
